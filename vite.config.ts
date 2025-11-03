fd5abb01ee11a71a96e7fe102575c013b4ae74c6
Dave Thompson <davecthompson@gmail.com>
2025-11-02 21:35:00 -0800

    Update styles and components

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Add this 'base' property
+  base: '/besthuman-portal-sandbox/',
   plugins: [react()],
 })
